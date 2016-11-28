172bdc052aec723492e31d304c148be65a3f1e75
Tzvi Friedman <tfriedman@learnvest.com>
2016-11-28 09:05:12 -0500

    remove alternate impl

--- binary-search/index.js
+++ binary-search/index.js
@@ -20,26 +20,5 @@ function searchB(arr,elem
 	}
 	return index;
 }
-function search(arr, ele) {
-  var len = arr.length;
-  if (len === 0) {
-    return -1;
-  }
 
-  function _search(startIdx, endIdx) {
-    if (startIdx === endIdx && arr[startIdx] !== ele) {
-      return -1;
-    }
-    var middleIdx = startIdx + Math.floor((endIdx - startIdx) / 2);
-    if (arr[middleIdx] > ele) {
-      return _search(startIdx, middleIdx);
-    } else if (arr[middleIdx] < ele) {
-      return _search(middleIdx + 1, endIdx);
-    } else {
-      return middleIdx;
-    }
-  }
-
-  return _search(0, len);
-}
 module.exports= searchB;
