a483d01ff745d4de1289c5c259bb721d7f8768c5
Steve Friedman <stevefriedman@northwesternmutual.com>
2018-10-18 09:05:32 -0400

    remove solutions

--- map/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (arr,func,ctx){
-	var newArr = [],
-		context = ctx || this;
-	for(var i = 0; i<arr.length; ++i ){
-		newArr.push(func.call(context,arr[i],i,arr))
-	}
-	return newArr;
-}
