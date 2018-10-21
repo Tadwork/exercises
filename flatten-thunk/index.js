02906acd847179acdf9ae1c2b55bc63c618775cf
Steve Friedman <stevefriedman@northwesternmutual.com>
2018-10-21 00:53:17 -0400

    flatten-thunk

--- flatten-thunk/index.js
+++ flatten-thunk/index.js
@@ -1,4 +1,13 @@
 
-module.exports = {
-    
+module.exports = function(thunk){
+    return function(cb){
+        const flatten =(err,result)=>{
+            if(typeof result === 'function'){
+                result(flatten)
+            }else{
+                cb.call(null,err,result)
+            }
+        }
+        thunk(flatten)
+    }
 }
