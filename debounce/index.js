dc17b218050e8d8afc5026a1a70a33d04723a830
Steve Friedman <stevefriedman@northwesternmutual.com>
2018-10-21 00:21:46 -0400

    debounce

--- debounce/index.js
+++ debounce/index.js
@@ -1,4 +1,18 @@
 
-module.exports = {
-    
+module.exports = function(func,time){
+    let canCall = true;
+    let context;
+    let lastArgs;
+    function debounced(...args){
+        context = this;
+        lastArgs = args;
+        if(canCall){
+            canCall = false
+            setTimeout(function(){
+                canCall = true;
+                func.apply(context,lastArgs)
+            },time)
+        }
+    }
+    return debounced;
 }
