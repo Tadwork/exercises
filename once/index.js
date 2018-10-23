e3d4c1b70073a69aa3ea6507de26636e0baad443
Steve Friedman <stevefriedman@northwesternmutual.com>
2018-10-23 11:15:01 -0400

    once

--- once/index.js
+++ once/index.js
@@ -1,4 +1,12 @@
 
-module.exports = {
-    
+module.exports = function(func){
+    let alreadyCalled = false
+    let result
+    return function(...args){
+        if(!alreadyCalled){
+            alreadyCalled = true
+            result = func.apply(this,args)
+        }
+        return result
+    }
 }
