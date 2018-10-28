af2af0f11a2a03d0d5caf288bbb4a3a64a6c5bfd
Steve Friedman <stevefriedman@northwesternmutual.com>
2018-10-27 23:32:18 -0400

    curry

--- curry/index.js
+++ curry/index.js
@@ -1,4 +1,13 @@
 
 module.exports = function(func){
-
+    const curried = function(...args){
+        let finalArgs = this.args ? [...this.args] : []
+        finalArgs.push(...args)
+        if(finalArgs.length >= func.length){
+            return func.apply(null,finalArgs)
+        }else{
+            return curried.bind({args: finalArgs})
+        }
+    }
+    return curried
 }
