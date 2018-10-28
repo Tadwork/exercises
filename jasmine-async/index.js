b89e2124ea52288a1a07def16151fdfd9042a987
Steve Friedman <stevefriedman@northwesternmutual.com>
2018-10-28 09:53:20 -0400

    jasmine-test

--- jasmine-async/index.js
+++ jasmine-async/index.js
@@ -1,4 +1,19 @@
 
 module.exports = function(func) {
-
-  }
+    let done
+    const {
+        desc,
+        setup,
+        test
+    } = func()
+    const doneFunc = ()=>done = true
+    it(desc, function() {
+        runs(()=>setup(doneFunc));
+      
+        waitsFor(function() {
+          return done;
+        }, 750);
+      
+        runs(test);
+      });
+}
