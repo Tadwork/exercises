61312965da32c5097fe259c378dae353c72ea130
Steve Friedman <stevefriedman@northwesternmutual.com>
2018-10-28 15:55:40 -0400

    middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,22 @@
 class Middleware{
-
+    constructor(){
+        this.middlewares = []
+    }
+    use(middleware){
+        this.middlewares.push(middleware)
+    }
+    go(doneFunc){
+        const runMiddleware = (index)=>{
+            if(index < this.middlewares.length){
+                this.middlewares[index].call(this,()=>{
+                    runMiddleware(index+1)
+                })
+            }else{
+                doneFunc.call(this)
+            }
+        }
+        runMiddleware(0)
+    }
 }
 
 module.exports = Middleware
