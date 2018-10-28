bcfcc8486539713d44f18f7ca8dea8f81b358f88
Steve Friedman <stevefriedman@northwesternmutual.com>
2018-10-28 09:56:28 -0400

    map

--- map/index.js
+++ map/index.js
@@ -1,4 +1,8 @@
 
 module.exports = function(arr,func,ctx = null){
-  
+    const newArr =[]
+    for(let i = 0;i< arr.length;i++){
+        newArr[i] = func.call(ctx,arr[i],i,arr)
+    }
+    return newArr
 }
