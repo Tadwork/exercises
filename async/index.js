942cc2d5f5bc73e38755ab088afb6f8ae7f60a7e
Steve Friedman <stevefriedman@northwesternmutual.com>
2018-10-27 23:06:32 -0400

    async and binary-search

--- async/index.js
+++ async/index.js
@@ -1,12 +1,64 @@
+/* utility functions */
+function promisify(func,val){
+    return new Promise((resolve,reject)=>{
+            const cb = (err,response)=>{
+                if(err){
+                    reject(err)
+                }else{
+                    resolve(response)
+                }
+            }
+            func(cb,val)
+        })
+}
+function runInSeq(arrayOfAsyncFunctions,upTo,val){
+    if(upTo < arrayOfAsyncFunctions.length){
+        return promisify(arrayOfAsyncFunctions[upTo],val).then((newVal)=>{
+            return runInSeq(arrayOfAsyncFunctions,upTo+1,newVal)
+        })
+    }else{
+        return Promise.resolve(val)
+    }
+}
+
+/* public functions */
 
 function sequence(arrayOfAsyncFunctions){
-  
+    return (doneFunc)=>{
+        runInSeq(arrayOfAsyncFunctions,0)
+        .then((val)=>{
+            doneFunc(null,val)
+        }).catch((err)=>{
+            doneFunc(err)
+        })
+    }
 }
 function parallel(arrayOfAsyncFunctions){
-
+    const results = []
+    return (doneFunc)=>{
+        const promises = arrayOfAsyncFunctions.map((func,i)=>{
+            return promisify(func).then((val)=>{
+                results[i] = val
+            })
+        })
+        Promise.all(promises).then(()=>{
+            doneFunc(null,results)
+        }).catch((err)=>{
+            doneFunc(err)
+        })
+    }
 }
 function race(arrayOfAsyncFunctions){
-
+    return (doneFunc)=>{
+        const promises = arrayOfAsyncFunctions.map((func,i)=>{
+            return promisify(func)
+        })
+        Promise.race(promises).then((val)=>{
+            doneFunc(null,val)
+        }).catch((err)=>{
+            doneFunc(err)
+        })
+    }
 }
 
 module.exports = {
