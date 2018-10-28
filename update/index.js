d4b5ab4fce9f7dfaf11e12769a86961bb251e5dd
Steve Friedman <stevefriedman@northwesternmutual.com>
2018-10-27 21:25:01 -0400

    update

--- update/index.js
+++ update/index.js
@@ -1,4 +1,34 @@
 
-module.exports = {
-    
+module.exports = function(state,mutations){
+    function change(obj,mutationAtLevel){
+        let copy;
+        if(Array.isArray(obj)){
+            copy = obj.slice()
+        }else if(typeof obj === 'object'){
+            copy = Object.assign({},obj)
+        }else{
+            copy = obj
+        }
+        for(let prop in mutationAtLevel){
+            if(prop === '$set'){
+                copy[prop] = copy =  mutationAtLevel[prop]
+            }else if(prop === '$push'){
+                copy.push.apply(copy,mutationAtLevel[prop])
+            }else if(prop === '$unshift'){
+                copy.unshift.apply(copy,mutationAtLevel[prop])
+            }else if(prop === '$splice'){
+                for(let i = 0; i< mutationAtLevel[prop].length; i++){
+                    copy.splice.apply(copy,mutationAtLevel[prop][i])
+                }
+            }else if(prop === '$merge'){
+                Object.assign(copy,mutationAtLevel[prop])
+            }else if(prop === '$apply'){
+                copy = mutationAtLevel[prop].call(null,copy)
+            }else{
+                copy[prop] = change(obj[prop],mutationAtLevel[prop])
+            }
+        }
+        return copy
+    }
+    return change(state,mutations)
 }
