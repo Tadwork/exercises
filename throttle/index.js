bece7d50626e8d2a9bee6f825cebe3965bfa086e
Steve Friedman <stevefriedman@northwesternmutual.com>
2018-10-29 18:37:52 -0400

    throttle

--- throttle/index.js
+++ throttle/index.js
@@ -1,4 +1,20 @@
 
 module.exports = function(func,time){
-
+    let calledOnce,
+        calledASecondTime,
+        lastArgs
+    return function throttle(){
+        if(!calledOnce){
+            calledOnce = true
+            func.apply(this,arguments)
+        }else if(!calledASecondTime){
+            calledASecondTime = true 
+            setTimeout(()=>{
+                calledOnce = false 
+                calledASecondTime = false
+                throttle.apply(this,lastArgs)
+            },time)
+        }
+        lastArgs = arguments
+    }
 }
