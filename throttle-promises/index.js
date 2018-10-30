2452352f362dd8a5fac1759e64b4a1396d4620c5
Steve Friedman <stevefriedman@northwesternmutual.com>
2018-10-29 22:39:40 -0400

    throttle-promises

--- throttle-promises/index.js
+++ throttle-promises/index.js
@@ -1,5 +1,27 @@
 
 
 module.exports = function(max, arr){
-
+    let running = 0,
+            upTo = 0,
+            results = []
+    return new Promise(function(resolve,reject){
+        function runNext(){
+            let promiseInd = upTo
+            if(promiseInd < arr.length){
+                arr[promiseInd]().then((val)=>{
+                    results[promiseInd] = val
+                    running--
+                    runNext()
+                    if(running === 0){
+                        resolve(results)
+                    }
+                })
+                upTo++
+                running++
+            }
+        }
+        while(running < max){
+            runNext()
+        }
+    })
 }
