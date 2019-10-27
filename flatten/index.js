c9eb014a6ab51ba3dec54219c04dc0f42760a449
Steve Friedman <stevefriedman@northwesternmutual.com>
2019-10-27 00:05:20 -0400

    flatten

--- flatten/index.js
+++ flatten/index.js
@@ -1,1 +1,18 @@
-
+/**
+ *
+ *
+ * @param {Array} array
+ * @returns {Array} a flattened array
+ */
+function flatten(array){
+  const flattened = []
+  array.forEach((item)=>{
+    if(Array.isArray(item)){
+      flattened.push(...flatten(item))
+    }else{
+      flattened.push(item)
+    }
+  })
+  return flattened
+}
+module.exports = flatten
