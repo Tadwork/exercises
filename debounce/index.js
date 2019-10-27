a787bd4a394bc109cca709bc236016fd56c67cad
Steve Friedman <stevefriedman@northwesternmutual.com>
2019-10-26 23:39:36 -0400

    debounce

--- debounce/index.js
+++ debounce/index.js
@@ -1,1 +1,25 @@
-
+/**
+ *
+ * returns a debounced function, preserves the context and argument from the last call
+ *
+ * @param {Function} func
+ * @param {Number} milliseconds
+ * @returns
+ */
+function debounce(func,milliseconds){
+  let waiting = false,
+      args,
+      ctx
+  return function(){
+    if(waiting === false){
+      waiting = true
+      setTimeout(()=>{
+          waiting = false
+          func.apply(ctx,args)
+      },milliseconds)
+    }
+    args = Array.from(arguments)
+    ctx = this
+  }
+}
+module.exports = debounce
