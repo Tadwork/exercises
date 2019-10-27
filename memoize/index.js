7d505edf19b85f42cc61ea8a3a2dd7c987540a3d
Steve Friedman <stevefriedman@northwesternmutual.com>
2019-10-27 01:37:34 -0400

    memoize

--- memoize/index.js
+++ memoize/index.js
@@ -1,1 +1,21 @@
+/**
+ *
+ *
+ * @param {Function} func
+ * @returns {Function} memoized function
+ */
+function memoize(func){
+  const memoizeMap = new Map()
+  return function(){
+    const argKey = Array.from(arguments).reduce((prev,arg)=>prev+"-"+arg)
+    if(memoizeMap.has(argKey)){
+      return memoizeMap.get(argKey)
+    }else{
+      const value = func.apply(this,arguments)
+      memoizeMap.set(argKey,value)
+      return value
+    }
+  }
+}
 
+module.exports  = memoize
