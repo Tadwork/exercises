69d82825a0a54f352024fb22f5bf2bc002730ce9
Steve Friedman <stevefriedman@northwesternmutual.com>
2019-10-26 23:14:12 -0400

    curry

--- curry/index.js
+++ curry/index.js
@@ -1,1 +1,20 @@
+/**
+ *
+ *
+ * @param {Function} func function to curry
+ * @returns {Function} curried function
+ */
+function curry(func){
+  function curried_func (){
+    const args = Array.isArray(this.args) ? this.args.slice() : []
+    args.push(...arguments)
+    if(args.length >= func.length ){
+      return func.apply(null,args)
+    }else{
+      return curried_func.bind({args})
+    }
+  }
+  return curried_func
+}
 
+module.exports = curry
