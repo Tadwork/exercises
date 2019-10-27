ae2131552b4cb40febee72296b5fdeafa5669535
Steve Friedman <stevefriedman@northwesternmutual.com>
2019-10-27 00:28:19 -0400

    flattenThunk

--- flatten-thunk/index.js
+++ flatten-thunk/index.js
@@ -1,1 +1,20 @@
+/**
+ *
+ *
+ * @param {Function} thunk
+ * @returns a function that will flatten all subsequent thunks
+ */
+function flattenThunk(thunk){
+    return (callback)=>{
+        const next = (err,data_or_thunk)=>{
+          if(typeof data_or_thunk === 'function' && !err){
+            data_or_thunk.call(this,next)
+          }else{
+            callback(err,data_or_thunk)
+          }
 
+        }
+        thunk.call(this,next)
+    }
+}
+module.exports = flattenThunk
