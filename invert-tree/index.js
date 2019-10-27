880328c2d2c458ede649a6aecacf14eacf5f1225
Steve Friedman <stevefriedman@northwesternmutual.com>
2019-10-27 01:09:17 -0400

    invert-tree

--- invert-tree/index.js
+++ invert-tree/index.js
@@ -1,1 +1,29 @@
 
+function invertTree(root){
+    function swap(node){
+      const temp = node.left
+      if(node.right){
+        node.left = node.right
+      }else{
+        // don't leave "undefined" because it breaks the deepEquals test
+        delete node.left
+      }
+      if(temp){
+        node.right = temp
+      }else{
+        // don't leave "undefined" because it breaks the deepEquals test
+        delete node.right
+      }
+    }
+    function traverse(node){
+      if(node){
+        swap(node)
+        traverse(node.left)
+        traverse(node.right)
+      }
+    }
+    traverse(root)
+
+}
+
+module.exports = invertTree
