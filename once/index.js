9019d0cf7244bf7e74628702420f04739bdec1b5
Steve Friedman <stevefriedman@northwesternmutual.com>
2019-11-04 01:39:37 -0500

    once

--- once/index.js
+++ once/index.js
@@ -1,1 +1,13 @@
+function once(func){
+  let memoized,called
+  // const ctx = this
+  return function(){
+    if(!called){
+      called = true
+      memoized = func.apply(this,arguments)
+    }
+    return memoized
+  }
+}
 
+module.exports = once
