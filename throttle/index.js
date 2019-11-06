d7ae04e2a37dc32e736e9e4e997920b61fadb1a6
Steve Friedman <stevefriedman@northwesternmutual.com>
2019-11-06 09:50:31 -0500

    throttle wip

--- throttle/index.js
+++ throttle/index.js
@@ -1,1 +1,29 @@
-
+/**
+ *
+ *
+ * @param {Function} func to throttle
+ * @param {number} duration how long to wait before we should fire if called again
+ * @returns
+ */
+function throttle(func,duration){
+    let called = 0,
+        args
+    return function throttled_func(){
+      //gets called with the later arguments
+      args = arguments
+      if(called === 0){
+        func.apply(this,args)
+        setTimeout(()=>{
+          called = 0
+        },duration)
+      }
+      // else if (called === 1){
+      //   setTimeout(()=>{
+      //     // will execute at least once more to make up for swallowed calls
+      //     throttled_func.apply(this,args)
+      //   },duration)
+      // }
+      called++
+    }
+}
+module.exports = throttle
